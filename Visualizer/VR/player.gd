26ea16d9298d6fd22c6961508d6d4005fcc11df7
Matthew Buckman <buckmanm@colostate.edu>
2024-03-11 19:51:30 -0600

    Added cord translation method

--- Visualizer/VR/player.gd
+++ Visualizer/VR/player.gd
@@ -2,12 +2,14 @@ extends Node3D
 var teleportPoint: Area3D
 var teleportMesh: Area3D
 var meshEntered: bool = false
-
+var teleportOrgin: Node3D
+var teleportPos: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	teleportPoint = get_node("XROrigin3D/Right Controller/MeshInstance3D/Area3D")
 	teleportMesh = get_node("XROrigin3D/Left Controller/MeshInstance3D/Area3D")
+	teleportOrgin = get_node("XROrigin3D/Left Controller/MeshInstance3D/TeleportMesh/TeleportOrgin")
 	pass # Replace with function body.
 
 
@@ -15,7 +17,20 @@ func _ready():
 func _process(delta):
 	if (meshEntered):
 		print_debug("area entered")
+		getPos()
 	pass 
+
+
+# TODO get position of teleport relative to 
+func getPos():
+	var pos = teleportPoint.global_position.distance_to(teleportOrgin.global_position)
+	var scale = teleportMesh.scale
+	# divide cordinates by the scale of our teleport mesh
+	pos.x = pos.x / scale.x
+	pos.y = pos.y / scale.y
+	pos.z = pos.z / scale.z
+	# when translating to world mesh we will multiply the points by it's scale
+	return pos
 
 
 # called when teleport point on right hand enters teleport mesh on right
