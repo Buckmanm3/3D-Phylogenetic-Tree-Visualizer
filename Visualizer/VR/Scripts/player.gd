2e72c37ba4c1686d3c1ccf780a7450e00e206c81
Matthew Buckman <buckmanm@colostate.edu>
2024-03-19 07:51:19 -0600

    added world transform code and timerNode

--- Visualizer/VR/Scripts/player.gd
+++ Visualizer/VR/Scripts/player.gd
@@ -4,21 +4,33 @@ var teleportMesh: Area3D
 var meshEntered: bool = false
 var teleportOrgin: Node3D
 var teleportPos: Vector3
-
+var timer: Timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	teleportPoint = get_node("XROrigin3D/Right Controller/MeshInstance3D/Area3D")
 	teleportMesh = get_node("XROrigin3D/Left Controller/MeshInstance3D/Area3D")
 	teleportOrgin = get_node("XROrigin3D/Left Controller/MeshInstance3D/TeleportMesh/TeleportOrgin")
-	pass # Replace with function body.
+	
+	# initalize timer to control how often we check the pos of teleport point
+	timer = Timer.new()
+	add_child(timer)
+	timer.autostart = true
+	timer.wait_time = 0.5  # timer will check every .5 seconds
+	timer.connect("timeout", _timeout)
+	
+	pass
 
+# called by timer node created in _ready()
+func _timeout():
+	if (meshEntered):
+		print_debug("area entered")
+		teleportPos = getPos()
+	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (meshEntered):
-		print_debug("area entered")
-		# getPos() currently crashes the application lol
-	pass 
+	pass
+	
 
 
 # TODO get position of teleport relative to 
@@ -26,9 +38,9 @@ func getPos():
 	var pos = teleportPoint.global_position.distance_to(teleportOrgin.global_position)
 	var scale = teleportMesh.scale
 	# divide cordinates by the scale of our teleport mesh
-	#pos.x = pos.x / scale.x
-	#pos.y = pos.y / scale.y
-	#pos.z = pos.z / scale.z
+	pos.x = pos.x / scale.x
+	pos.y = pos.y / scale.y
+	pos.z = pos.z / scale.z
 	# when translating to world mesh we will multiply the points by it's scale
 	return pos
 
--- Visualizer/World.gd
+++ Visualizer/World.gd
@@ -1,20 +1,54 @@
 extends Node3D
 
 var interface : XRInterface
+var pointPos: Vector3
+var timer: Timer 
+var meshEntered: bool
+var projection: MeshInstance3D
+var teleportMesh: Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	# initalize VR
 	get_viewport().use_xr = true
 	interface = XRServer.find_interface("OpenXR")
 	if interface and interface.is_initialized():
 		print("XR scene correctly launched")
 	else:
 		print("Error VR incorrectly launched")
-
+		
+	# get refrences to player's script variables
+	var player = get_node("Player")
+	meshEntered = player.meshEntered
+	pointPos = player.teleportPos
+	timer = player.timer
+	timer.connect("timeout", _timeout)
+	
+	# get refrences to child nodes
+	projection = get_node("TeleportProjection")
+	teleportMesh = get_node("TeleportMesh")
+	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+# called when player tiemr runs out
+func _timeout():
+	if (meshEntered):
+		projection.position = convertPos()
+		projection.visible = true
+	else:
+		projection.visible = false
 
-
+# transforms point cords to align with world mesh
+func convertPos():
+	var newPos: Vector3
+	var scale = teleportMesh.scale
+	newPos.x = pointPos.x * scale.x
+	newPos.y = pointPos.y * scale.y
+	newPos.z = pointPos.z * scale.z
+	return newPos
+	
